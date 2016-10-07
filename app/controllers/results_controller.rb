a5cec14a20042f9fef729d8510977f0fe6045903
Ryker <ryker.reed3@gmail.com>
2016-10-07 13:36:01 -0700

    Adds model, view, controller for results to get basic query result rendered on page.

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,52 @@
+require 'oauth'
+require 'json'
+require 'pp'
+require 'csv'
+require 'erb'
+require 'dotenv'
+Dotenv.load
+
+erb = ERB.new(File.open("github-milesgrimshaw-yelp.html.erb").read)
+
+class ResultsController < ApplicationController
+
+	def index
+
+		consumer_key = ENV['CONSUMER_KEY']
+		consumer_secret = ENV['CONSUMER_SECRET']
+		token = ENV['TOKEN']
+		token_secret = ENV['TOKEN_SECRET']
+
+		api_host = 'api.yelp.com'
+
+		consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+		access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+		
+	    path = "/v2/search?term=yoga&location=oakland"
+
+	    result = JSON.parse(access_token.get(path).body)
+	    # p "*" * 50
+	    @ccount = result["total"]
+
+
+		# cities = [
+		# "New+York", "Los+Angeles", "Chicago", "Houston", "Philadelphia", "Pheonix", "San+Antonio", "San+Diego", "Dallas", "San+Jose", "Austin", "Jacksonville",
+		# "Indianapolis", "San+Francisco", "Columbus", "Fort+Worth", "Charlotte", "Detroit", "El+Paso", "Memphis", "Boston", "Seattle", "Denver", "Washington",
+		# "Nashville", "Baltimore", "Louisville", "Portland", "Oklahoma+City", "Milwaukee", "Las+Vegas", "Albuquerque", "Tucson", "Fresno", "Sacramento", 
+		# "Long+Beach", "Kansas+City", "Mesa", "Virginia+Beach", "Atlanta","Colorado+Springs", "Raleigh", "Omaha", "Miami", "Oakland", "Tulsa", "Minneapolis", 
+		# "Cleveland", "Wichita", "Arlington", "New+Orleans", "Bakersfield", "Tampa", "Honolulu", "Anaheim", "Aurora", "Santa+Ana", "St.+Louis,MA", "Riverside", 
+		# "Corpus+Christi", "Pittsburgh"
+		# ]
+		cities = []
+
+		segments = [
+		"fitness+class", "bootcamp", "crossfit+gym", "yoga+class", "pilates+class", "martial+arts+class", "spin+class", 
+		"biking+class", "boxing", "ballet+class", "barre+class"
+		]
+
+		@result = Result.new({city: "oakland", item: "yoga", count: @ccount})
+		@result.save
+		@result_count = @result.count
+		# @result_json = Result.test_var
+	end
+end
